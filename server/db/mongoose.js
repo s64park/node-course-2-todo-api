6b17d38d95c11262b35036106a0c56495c432bc0
s64park <s64park@uwaterloo.ca>
2017-01-02 01:43:41 -0500

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 // save new something
 
 module.exports = { mongoose };
+
